897e7c0fcd982effb92e7d3e14a4609ad688960e
bootcamp-harisenin <sekarqonita123@gmail.com>
2024-01-14 20:42:12 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let count = 0;
+
+function countUp() {
+  document.getElementById("count").textContent = ++count;
+}
+
+function countDown() {
+  document.getElementById("count").textContent = --count;
+}
